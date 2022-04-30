3fe73351a2026bb2988749ee33c8ac5407b5c839
thomasrsoltvedt <81139327+thomasrsoltvedt@users.noreply.github.com>
2022-04-30 15:30:57 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -39,33 +39,23 @@ getProducts(); */
 const menProducts = document.querySelector(".men-products"); 
 const womenProducts = document.querySelector(".women-products"); 
 
-const url = "http://rainydays.local/wp-json/wc/store/products?categories"; 
+const url = "http://rainydays.local/wp-json/wc/store/products"; 
 
 async function getProduct() {
     const response = await fetch(url); 
     const results = await response.json(); 
 
     for (let i = 0; i < results.length; i++) {
-        if (results[i].categories[0].id === 16) {
-            womenProducts.innerHTML += `
-            <a href="details.html?id=${results[i].id}"><div class="product-card">
-                <img src="${results[i].images[0].src}">
-                <h2>${results[i].name}</h2>
-                <p>€${results[i].prices.price}</p>
-            </div></a>
-            `;
-        } else if (results[i].categories[0].id === 17) {
-            menProducts.innerHTML += `
-            <a href="details.html?id=${results[i].id}"><div class="product-card">
-                <img src="${results[i].images[0].src}">
-                <h2>${results[i].name}</h2>
-                <p>€${results[i].prices.price}</p>
-            </div></a>
-            `;
-        }
+        menProducts.innerHTML += `
+        <a href="details.html?id=${results[i].id}"><div class="product-card">
+            <img src="${results[i].images[0].src}">
+            <h2>${results[i].name}</h2>
+            <p>€${results[i].prices.price}</p>
+        </div></a>
+        `;
     }
 
-    console.log(results[0].categories[0].id);
+    console.log(results[0]);
 }
 
 getProduct();
